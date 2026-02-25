8a9a49a27ece54e786328fc66dbe2b6620c082c6
Arun Reddy E <arun_reddy1@epam.com>
2026-02-26 00:27:02 +0530

    Collections task3

---
+++ src/main/java/org/example/Collections/Task3/Main.java
@@ -0,0 +1,29 @@
+package org.example.Collections.Task3;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(sc.nextLine());
+        Map<String, String> phoneBook = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String name = sc.nextLine();
+            String phone = sc.nextLine();
+            phoneBook.put(name, phone);
+        }
+
+        while (sc.hasNextLine()) {
+            String query = sc.nextLine();
+            if (phoneBook.containsKey(query)) {
+                System.out.println(query + "=" + phoneBook.get(query));
+            } else {
+                System.out.println("Not_found");
+            }
+        }
+
+        sc.close();
+    }
+}
